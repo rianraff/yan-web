aed735e38913afc2b4675dc712ae957d1597b82b
rianraff <rianraffi@gmail.com>
2025-02-16 00:14:28 +0700

    finish the first screen

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+export default function Header() {
+    return (
+        <nav className="bg-stone-50 border-b border-stone-900">
+            <div className="mx-auto px-2 flex max-h-16 items-center text-stone-900 font-[var(--font-roboto-mono)]">
+                <div className="mx-8 my-8">
+                    <span className="text-black text-2xl font-bold">yan.</span>
+                </div>
+                <div className="mx-6 space-x-16 text-sm">
+                    <a>Home</a>
+                    <a>Projects</a>
+                    <a>Playground</a>
+                    <a>Blogs</a>
+                    <a>Contact</a>
+                </div>
+                <div className="ml-auto text-sm mx-4">
+                    <button className="border border-stone-900 px-4 py-1">Let's Connect</button>
+                </div>
+            </div>
+        </nav>
+    );
+}
